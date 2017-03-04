1b454d55f931a995b874d479cf2ef3846c026e30
JEISON ARENALES <jeison.arenales@gmail.com>
2017-03-04 12:47:23 -0500

    Fix in regular expresion

--- application/forms/Cart/Authorizenet/Checkout.php
+++ application/forms/Cart/Authorizenet/Checkout.php
@@ -1,17 +1,17 @@
 <?php
 /**
-* 
+*
 */
 class Form_Cart_Authorizenet_Checkout extends My_Form_Abstract
 {
-	
+
 	public function init()
 	{
 		parent::init();
 		$this->setAttrib('id', 'checkout-form');
 		$this->setAttrib('class', 'checkout-form');
 		$this->setDescription('Enter Your Billing Information');
-		
+
 		$this->setDecorators(array(
 			array(
 				'ViewScript',
@@ -19,19 +19,19 @@ class Form_Cart_Authorizenet_Checkout extends My_Form_Abstract
 			),
 			'Form'
 		));
-		
+
 		$this->addElement('text', 'first_name', array(
 			'label' => 'First Name:',
 			'required' => true,
 			'errorMessages' => array('Please enter your first name')
 		));
-		
+
 		$this->addElement('text', 'last_name', array(
 			'label' => 'Last Name:',
 			'required' => true,
 			'errorMessages' => array('Please enter your last name')
 		));
-		
+
 		$this->addElement('text', 'email', array(
 			'label' => 'Email:',
 			'required' => true,
@@ -42,23 +42,23 @@ class Form_Cart_Authorizenet_Checkout extends My_Form_Abstract
 			),
 			'errorMessages' => array('Please enter a valid email address')
 		));
-		
+
 		$this->addElement('text', 'phone', array(
 			'label' => 'Phone:',
 			'required' => true,
 			'errorMessages' => array('Please enter a contact phone number')
 		));
-		
+
 		$this->addElement('text', 'address', array(
 			'label' => 'Address:',
 			'required' => true,
 			'filters' => array('StringTrim'),
 			'validators' => array(
-				array('Regex', false, array('/^[a-zA-Z0-9\.,-_ ]+$/i'))
+				array('Regex', false, array('/^[a-zA-Z0-9\.\,\-\_\ ]+$/i'))
 			),
 			'errorMessages' => array('Please enter your address')
 		));
-		
+
 		$this->addElement('text', 'city', array(
 			'label' => 'City:',
 			'required' => true,
@@ -68,9 +68,9 @@ class Form_Cart_Authorizenet_Checkout extends My_Form_Abstract
 			),
 			'errorMessages' => array('Please enter your city name')
 		));
-		
+
 		$states = new My_Static_State();
-		
+
 		$this->addElement('select', 'state', array(
 			'label' => 'State:',
 			'required' => true,
@@ -81,27 +81,27 @@ class Form_Cart_Authorizenet_Checkout extends My_Form_Abstract
 			'errorMessages' => array('Please enter your state abbreviation'),
 			'multiOptions' => $states->toArray()
 		));
-		
+
 		$this->addElement('text', 'zip', array(
 			'label' => 'Zip Code:',
 			'required' => true,
 			'filters' => array(
 				'StringTrim'
-			), 
+			),
 			'validators' => array(
 				array('StringLength', false, array(5,5)) ,
 				'Digits',
 			),
 			'errorMessages' => array('Please enter a 5 digit zipcode')
 		));
-		
+
 		// $this->addElement('text', 'country', array(
 		// 			'label' => 'Country:',
 		// 			'required' => true,
 		// 			'errorMessages' => array('Please enter your country')
 		// 		));
-		
-		
+
+
 		$this->addElement('text', 'card_num', array(
 			'label' => 'Card Number',
 			'required' => true,
@@ -110,7 +110,7 @@ class Form_Cart_Authorizenet_Checkout extends My_Form_Abstract
 			),
 			'errorMessages' => array('Please enter your credit card number')
 		));
-		
+
 		$this->addElement('text', 'exp_date', array(
 			'label' => 'Exp. (MM/YY)',
 			'required' => true,
@@ -119,7 +119,7 @@ class Form_Cart_Authorizenet_Checkout extends My_Form_Abstract
 			),
 			'errorMessages' => array('Please enter expiration in format "mm/yy"'),
 		));
-		
+
 		$this->addElement('text', 'card_code', array(
 			'label' => 'Security Code',
 			'required' => true,
@@ -132,61 +132,61 @@ class Form_Cart_Authorizenet_Checkout extends My_Form_Abstract
 			),
 			'errorMessages' => array('Please enter the security code from the back of the card')
 		));
-		
-		
+
+
 		/*
 			Add Elements to Display Groups
 		*/
 		$this->addDisplayGroup(
-			array('first_name', 'last_name', 'email', 'phone'), 
-			'person-info', 
+			array('first_name', 'last_name', 'email', 'phone'),
+			'person-info',
 			array(
-				// 'legend' => 'Account Settings', 
+				// 'legend' => 'Account Settings',
 				'decorators' => array(
-					'FormElements', 
-					'Fieldset', 
+					'FormElements',
+					'Fieldset',
 					array('Description', array('placement' => 'prepend', 'tag' => 'div')),
 					array('HtmlTag', array('tag' => 'div', 'class' => 'checkout-group')),
 				),
 				'description' => 'Personal Details',
 			)
 		);
-		
+
 		$this->addDisplayGroup(
-			array('address', 'city', 'state', 'zip', 'country'), 
-			'address-info', 
+			array('address', 'city', 'state', 'zip', 'country'),
+			'address-info',
 			array(
-				// 'legend' => 'Account Settings', 
+				// 'legend' => 'Account Settings',
 				'decorators' => array(
-					'FormElements', 
-					'Fieldset', 
+					'FormElements',
+					'Fieldset',
 					array('Description', array('placement' => 'prepend', 'tag' => 'div')),
 					array('HtmlTag', array('tag' => 'div', 'class' => 'checkout-group')),
 				),
 				'description' => 'Billing Address',
 			)
 		);
-		
+
 		$this->addDisplayGroup(
-			array('card_num', 'exp_date', 'card_code'), 
-			'payment-info', 
+			array('card_num', 'exp_date', 'card_code'),
+			'payment-info',
 			array(
-				// 'legend' => 'Account Settings', 
+				// 'legend' => 'Account Settings',
 				'decorators' => array(
-					'FormElements', 
-					'Fieldset', 
+					'FormElements',
+					'Fieldset',
 					array('Description', array('placement' => 'prepend', 'tag' => 'div')),
 					array('HtmlTag', array('tag' => 'div', 'class' => 'checkout-group')),
 				),
 				'description' => 'Payment Details',
 			)
 		);
-		
+
 		$this->addElement('checkbox', 'confirm', array(
 			'label' => 'Agree to Terms',
 			'required' => true,
 		));
-		
+
 		$this->addElement('submit', 'submit', array(
 			'label' => 'Pay Now',
 			'decorators' => array(
@@ -194,7 +194,7 @@ class Form_Cart_Authorizenet_Checkout extends My_Form_Abstract
 				array('HtmlTag', array('tag' => 'div', 'class' => 'form-submit'))
 			)
 		));
-		
+
 		$this->setCustomDecorators();
 	}
 }
